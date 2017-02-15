7b8cbf3a08a7d530aa45870e4a43103579463456
Anthony Skelton <anthony@Anthonys-MacBook-Pro.local>
2017-02-15 09:40:10 -0800

    initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+## 1.0.1
+
+- distFilename configuration added
+
+## 15.Oct.2017
+
+Initial release.
---
+++ bootstrap.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Collapsible Content Plugin
+ *
+ * @package   Ajskelton\CollapsibleContent
+ * @since     1.0.0
+ * @author    ajskelton
+ * @link      anthonyskelton.com
+ * @license   GNU General Public License 2.0+
+ *
+ * @wordpress-plugin
+ * Plugin Name: Collapsible Content
+ * Plugin URI: _
+ * Description: Collapsible Content is a WordPress plugin that shows and hides hidden content.
+ * Version: 1.0.0
+ * Author: ajskelton
+ * Author URI: https://anthonyskelton.com
+ * Text Domain: collapsible_content
+ * Requires WP: 4.7
+ * Requires PHP: 5.5
+ *
+ */
+namespace Ajskelton\CollapsibleContent;
+
