7b9d438bbbb12b9cdd7642f3dc456b243078f81f
Anthony Skelton <anthony@Anthonys-MacBook-Pro.local>
2017-02-21 08:58:18 -0800

    topic + archive template

---
+++ src/faq/shortcode/shortcode.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Description
+ *
+ * @package   Ajskelton\Module\FAQ\Shortcode
+ * @since     1.0.0
+ * @author    ajskelton
+ * @link      anthonyskelton.com
+ * @license   GNU General Public License 2.0+
+ */
+namespace Ajskelton\Module\FAQ\Shortcode;
